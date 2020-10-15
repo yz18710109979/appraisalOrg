633c6b1a7a0f3585073eeb1da25f15d79f367619
“e <1922375530@qq.com>
2020-10-15 10:15:20 +0800

    git

---
+++ appraisalOrg-Calc/src/main/java/com/jingyou/CalcApp.java
@@ -0,0 +1,12 @@
+package com.jingyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcApp.class, args);
+    }
+}
